f3b6e673dda2746ee945a2f97044921257b687bd
Nicolás Barona Riera <nbaronariera@gmail.com>
2025-10-21 14:00:11 +0200

    Fix: cosas del git

--- Backend/src/main/kotlin/org/pin/backend/service/CasaService.kt
+++ Backend/src/main/kotlin/org/pin/backend/service/CasaService.kt
@@ -1,4 +1,5 @@
 package org.pin.backend.service
+import org.pin.backend.model.Casa
 import org.pin.backend.repository.CasaRepository
 import org.springframework.stereotype.Service
 
@@ -7,4 +8,6 @@ class CasaService(
     private val repo: CasaRepository,
 ) {
     fun findAll() = repo.findAll()
+
+    fun save(casa : Casa) = repo.save(casa)
 }
