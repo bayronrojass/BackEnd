371e21e48acb9104b42819e1881602dcbf5c6b9e
Nicolás Barona Riera <nbaronariera@gmail.com>
2025-10-26 13:46:25 +0100

    Lucas...

---
+++ Backend/src/main/kotlin/org/pin/backend/configs/WebConfig.kt
@@ -0,0 +1,15 @@
+package org.pin.backend.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+
+    // Esto hace que http://tu-servidor:8080/uploads/archivo.jpg funcione
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/uploads/**")
+            .addResourceLocations("file:./uploads/")
+    }
+}
