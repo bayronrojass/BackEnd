b91710d5af3fbed4f1afbb2940f1fc6e906062df
Nicolás Barona Riera <nbaronariera@gmail.com>
2025-10-26 13:50:00 +0100

    Eliminado Casa

--- Backend/src/main/kotlin/org/pin/backend/repository/CasaRepository.kt
+++
@@ -1,5 +0,0 @@
-package org.pin.backend.repository
-import org.pin.backend.model.Casa
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface CasaRepository : JpaRepository<Casa, Long>
