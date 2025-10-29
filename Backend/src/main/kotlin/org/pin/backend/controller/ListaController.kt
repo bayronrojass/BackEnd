cf61ee13c6a400b06a9b6d902b7179ea15b11598
bayronrojass <bayronrojas30@gmail.com>
2025-10-29 19:08:57 +0100

    Añadida Lógica Lista de elementos

--- Backend/src/main/kotlin/org/pin/backend/controller/ListaController.kt
+++ Backend/src/main/kotlin/org/pin/backend/controller/ListaController.kt
@@ -1,12 +1,31 @@
 package org.pin.backend.controller
+import org.pin.backend.model.Elemento
+import org.pin.backend.model.Lista
+import org.pin.backend.repository.ListaRepository
 import org.pin.backend.service.ListaService
+import org.springframework.http.ResponseEntity
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @RequestMapping("/listas")
 class ListaController(
-    private val service: ListaService,
+    private val listaRepository: ListaRepository,
 ) {
     @GetMapping
-    fun getAll() = service.findAll()
+    fun getAll() = listaRepository.findAll()
+
+    @GetMapping("/{listaId}/elementos")
+    @Transactional(readOnly = true)
+    fun getElementosByListaId(@PathVariable listaId: Long): ResponseEntity<List<Elemento>> {
+        val listaOptional: Optional<Lista> = listaRepository.findById(listaId)
+        return if (listaOptional.isPresent) {
+            val lista = listaOptional.get()
+            // Accedemos a lista.elementos DENTRO de la transacción
+            ResponseEntity.ok(lista.elementos.toList()) // Devuelve la lista de elementos
+        } else {
+            ResponseEntity.notFound().build() // Devuelve 404 si la lista no existe
+        }
+    }
 }
