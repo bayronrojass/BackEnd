6548fee7c4694712b1f2ab870f55fb8473848846
bayronrojass <bayronrojas30@gmail.com>
2025-12-10 10:57:04 +0100

    Más tareas en base de datos y cambio de listas

--- Backend/src/main/resources/import.sql
+++ Backend/src/main/resources/import.sql
@@ -148,6 +148,9 @@ INSERT INTO public.tarea (id, fecha_fin, frecuencia, periodica, casa_id, asignad
 INSERT INTO public.tarea (id, fecha_fin, frecuencia, periodica, casa_id, asignadoa_id, completado, nombre, prioridad) VALUES (10, NOW() + INTERVAL '1 day', 'Diaria', TRUE, 1, 1, false, 'Cargar el lavaplatos', 'Media') ON CONFLICT (id) DO NOTHING;
 INSERT INTO public.tarea (id, fecha_fin, frecuencia, periodica, casa_id, asignadoa_id, completado, nombre, prioridad) VALUES (11, NOW() + INTERVAL '3 days', 'Semanal', TRUE, 1, 2, false, 'Ir a la lavandería', 'Baja') ON CONFLICT (id) DO NOTHING;
 INSERT INTO public.tarea (id, fecha_fin, frecuencia, periodica, casa_id, asignadoa_id, completado, nombre, prioridad) VALUES (12, NOW() + INTERVAL '7 days', 'Mensual', FALSE, 1, 3, false,'Pasar la aspiradora', 'Baja') ON CONFLICT (id) DO NOTHING;
+INSERT INTO public.tarea (id, fecha_fin, frecuencia, periodica, casa_id, asignadoa_id, completado, nombre, prioridad) VALUES (13, now() - INTERVAL '4 days', 'Semanal', TRUE, 1, 1, true, 'Sacar al perro', 'Baja') ON CONFLICT (id) DO NOTHING;
+INSERT INTO public.tarea (id, fecha_fin, frecuencia, periodica, casa_id, asignadoa_id, completado, nombre, prioridad) VALUES (14, now() - INTERVAL '7 days', 'Mensual', FALSE, 1, 2, true, 'Limpiar Cocina', 'Alta') ON CONFLICT (id) DO NOTHING;
+INSERT INTO public.tarea (id, fecha_fin, frecuencia, periodica, casa_id, asignadoa_id, completado, nombre, prioridad) VALUES (15, now() - INTERVAL '15 days', 'Diaria', TRUE, 1, 3, true, 'Reunion propietaria', 'Media') ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO public.post_it (id, lienzo_id) VALUES (1, 4) ON CONFLICT (id) DO NOTHING;
 INSERT INTO public.post_it (id, lienzo_id) VALUES (2, 5) ON CONFLICT (id) DO NOTHING;
